84c1842164f6bb61e29585a6bdf2a87922b87250
NISHANT KUMAR <120430630+nik80760@users.noreply.github.com>
2023-08-30 01:54:38 +0530

    Add files via upload

---
+++ StudentManagementSystem2Application.java
@@ -0,0 +1,13 @@
+package net.javaguides.sms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentManagementSystem2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentManagementSystem2Application.class, args);
+	}
+
+}
---
+++ StudentService.java
@@ -0,0 +1,28 @@
+package net.javaguides.sms.service;
+
+import java.util.List;
+
+import net.javaguides.sms.entity.Student;
+
+public interface StudentService {
+ List<Student> getAllStudents();
+ 
+ Student saveStudent(Student student);
+ 
+ Student getStudentById(Long id);
+ 
+ Student updateStudent(Student student);
+ 
+ 
+ 
+ void deleteStudentById(Long id);
+ void deleteAllStudents();
+
+ Student uploadStudent(Student student);
+ 
+    List<Student> saveBulkStudents(List<Student> students);
+    
+    Student registerUser(String login,String password,String email1);
+
+    Student authenticate(String login,String password);
+}
